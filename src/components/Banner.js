18e941a152e16bba23b16876816d7d250b3b2b2f
50vincents <vincethai035@gmail.com>
2020-10-15 15:10:03 -0700

    finished and deployed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import '../styles/Banner.css'
 import { Button } from '@material-ui/core';
 import Search from './Search'
+import { useHistory } from 'react-router-dom';
  
 function Banner() {
+  const history = useHistory();
   const [showSearch, setShowSearch] = useState(false);
 
   return (
@@ -22,7 +24,7 @@ function Banner() {
       <div className='banner-info'>
         <h1>Get out and stretch your imagination</h1>
         <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
-        <Button variant='outlined'>Explore Nearby</Button>
+        <Button variant='outlined' onClick={() => history.push('/search')}>Explore Nearby</Button>
       </div>
     </div>
   )
