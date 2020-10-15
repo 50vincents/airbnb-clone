8c017538d8842dc99be0a9e70dcb2376b81e6fbc
50vincents <vincethai035@gmail.com>
2020-10-15 14:29:52 -0700

    added routing and search page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import SearchPage from './components/SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+
+          <Route path='/'>
+            <Home />
+          </Route>
+
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
